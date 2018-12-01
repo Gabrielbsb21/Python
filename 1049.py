f33cac232f340de14f13afe827184ef62a3fa703
Gabriel Teixeira <gabrielbsb21@outlook.com>
2018-12-01 11:41:05 -0200

    URI

---
+++ 1049.py
@@ -0,0 +1,21 @@
+palavra1 = str(input());
+palavra2 = str(input());
+palavra3 = str(input());
+if palavra1 == "vertebrado" and palavra2 == "ave" and palavra3 == "carnivoro":
+    print("aguia");
+elif palavra1 == "vertebrado" and palavra2 == "ave" and palavra3 == "onivoro":
+    print("pomba");
+elif palavra1 == "vertebrado" and palavra2 == "mamifero" and palavra3 == "onivoro":
+    print("homem");
+elif  palavra1 == "vertebrado" and palavra2 == "mamifero" and palavra3 == "herbivoro":
+    print("vaca");
+elif palavra1 == "invertebrado" and palavra2 == "inseto" and palavra3 == "hematofago":
+    print("pulga");
+elif palavra1 == "invertebrado" and palavra2 == "inseto" and palavra3 == "herbivoro":
+    print("lagarta");
+elif palavra1 == "invertebrado" and palavra2 == "anelideo" and palavra3 == "hematofago":
+    print("sanguessuga");
+elif palavra1 == "invertebrado" and palavra2 == "anelideo" and palavra3 == "onivoro":
+    print("minhoca");
+    
+    
---
+++ 1070.py
@@ -0,0 +1,10 @@
+num = int(input());
+if num % 2 == 0:
+  num += 1;
+  for _ in range(6):
+   print(num);
+   num += 2;
+else:
+  for _ in range(6):
+   print(num);
+   num += 2;
