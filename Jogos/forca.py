2e574d06548920ada39aad73aa8e7d21102fb588
Gabriel Teixeira <gabrielbsb21@outlook.com>
2019-04-13 16:39:21 -0300

    Em_Construção

---
+++ Jogos/forca.py
@@ -0,0 +1,11 @@
+'''
+Esse é um jogo da forca bem simples, mais pra frente deixaremos a palavra secreta mais dinâmica
+'''
+print('**********************')
+print('***Bem vindo ao jogo da Forca!***')
+print('**********************')
+
+palavra_secreta = 'banana'
+acertou = False
+enforcou = False
+print("Fim do jogo")
