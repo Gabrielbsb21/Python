ebd871be238c3dc1c5bafdce05dcb22543004926
Gabriel Teixeira da Silva <41963787+Gabrielbsb21@users.noreply.github.com>
2019-08-13 09:36:53 -0300

    Update main.py

--- CalculadoraPintura/main.py
+++ CalculadoraPintura/main.py
@@ -1,7 +1,7 @@
 largura = float(input('Qual o largura do cômodo? '))
 altura = float(input('Qual a altura do cômodo? '))
 profundidade = float(input('Qual a profundidade do cômodo? '))
-
+teste = "aceito"
 area_paredes = float =  (2 * (largura + profundidade) * altura)
 
 print(f'A area das paredes é: {area_paredes}')
