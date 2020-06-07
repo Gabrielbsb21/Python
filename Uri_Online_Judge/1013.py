a2bcb9fcfbdeae8af46e4c4ac6ed91c9b4b64432
Gabriel Teixeira da Silva <41963787+Gabrielbsb21@users.noreply.github.com>
2020-06-07 18:54:36 -0300

    Update

--- Uri_Online_Judge/1013.py
+++ Uri_Online_Judge/1013.py
@@ -1,7 +1,10 @@
-num1,num2,num3 = input().split(" ");
-num1 = int(num1);
-num2 = int(num2);
-num3 = int(num3);
-aux = (num1 + num2 + (abs(num1 - num2)))/2;
-maior = (aux + num3 + (abs(aux - num3)))/2;
-print("%d eh o maior" %maior);
+num_1, num_2, num_3 = input().split(' ')
+num_1 = int(num_1)
+num_2 = int(num_2)
+num_3 = int(num_3)
+maior_ab = int((num_1 + num_2 + abs(num_1 - num_2)) / 2)
+#resultado = print(f'{maior_ab} eh o maior') if(maior_ab > num_3) else print(f'{num_3} eh o maior')
+if maior_ab > num_3:
+    print(f'{maior_ab} eh o maior')
+else:
+    print(f'{num_3} eh o maior')
